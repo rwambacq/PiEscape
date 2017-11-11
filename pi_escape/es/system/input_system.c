bca554a0204a6a24382afa47f068910b0c64b307
thvkerck <Thomas.Vankerckhoven@UGent.be>
2017-11-11 23:38:05 +0100

    opnieuwwerkend

--- pi_escape/es/system/input_system.c
+++ pi_escape/es/system/input_system.c
@@ -11,9 +11,9 @@
 #include "../game_util.h"
 
 InputSystem* system_input_alloc() {
-    InputSystem* res = calloc(1, sizeof(InputSystem));
-    system_input_init(res);
-    return res;
+	InputSystem* res = calloc(1, sizeof(InputSystem));
+	system_input_init(res);
+	return res;
 }
 
 void system_input_init(InputSystem* system) {
@@ -21,132 +21,180 @@ void system_input_init(InputSystem* system) {
 
 
 void system_input_free(InputSystem* system) {
-    
+
 }
 
 static void handleKeyDown(InputSystem* system, Engine* engine, SDL_keysym *keysym, EntityId input_recv_entity_id)
 {
-    switch( keysym->sym ) {
-        case SDLK_ESCAPE:
-            //ignore untile key released
-            break;
-        case SDLK_KP_ENTER: //fall-through
-        case SDLK_RETURN:   //fall-through
-        case SDLK_SPACE: {
-            engine->context.demo = !engine->context.demo;
-            break;
-        }
-        default:
-            break;
-    }
-    
-    
-    
-    
+	switch (keysym->sym) {
+	case SDLK_ESCAPE:
+		//ignore untile key released
+		break;
+	case SDLK_KP_ENTER: //fall-through
+	case SDLK_RETURN:   //fall-through
+	case SDLK_SPACE: {
+		//engine->context.demo = !engine->context.demo;
+		break;
+	}
+	default:
+		break;
+	}
+
+
+
+
 }
 
 static void handleKeyUp(InputSystem* system, Engine* engine, SDL_keysym *keysym, EntityId inputReceiverEntity)
 {
-    switch( keysym->sym ) {
-        case SDLK_ESCAPE:
-            engine->context.is_exit_game = 1;
-            break;
-        case SDLK_UP:{
-            engine->context.demo = !engine->context.demo;
-            break;
-        }
-        case SDLK_DOWN:{
-            engine->context.demo = !engine->context.demo;
-            break;
-        }
-        case SDLK_LEFT:{
-            engine->context.demo = !engine->context.demo;
-            break;
-        }
-        case SDLK_RIGHT:{
-            engine->context.demo = !engine->context.demo;
-            break;
-        }
-        default:
-            break;
-    }
-    
-    
+	EntityIterator player_it;
+	EntityIterator itdoor;
+	search_entity_2(engine, COMP_GRIDLOCATION, COMP_INPUTRECEIVER, &player_it);
+	next_entity(&player_it);
+	EntityId player_entity_id = player_it.entity_id;
+	assert(player_entity_id != NO_ENTITY);
+	if (!has_component(engine, player_entity_id, COMP_BLOCKING)) {
+		switch (keysym->sym) {
+		case SDLK_o:
+			//key die deuren opent of sluit, handig voor debug
+			search_entity_3(engine, COMP_ART, COMP_ISDOOR, COMP_DIRECTION, &itdoor);
+			while (next_entity(&itdoor)) {
+				EntityId door = itdoor.entity_id;
+				assert(door != NO_ENTITY);
+				ActivatableComponent* xx = get_component(engine, door, COMP_ACTIVATABLE);
+				xx->active = !xx->active;
+			}
+			break;
+		case SDLK_ESCAPE:
+			engine->context.is_exit_game = 1;
+			break;
+		case SDLK_UP: {
+			//engine->context.demo = !engine->context.demo;
+			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
+			move->x_min_move = 1;
+			break;
+		}
+		case SDLK_DOWN: {
+			//engine->context.demo = !engine->context.demo;
+			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
+			move->x_plus_move = 1;
+			break;
+		}
+		case SDLK_LEFT: {
+			//engine->context.demo = !engine->context.demo;
+			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
+			move->y_plus_move = 1;
+			break;
+		}
+		case SDLK_RIGHT: {
+			//engine->context.demo = !engine->context.demo;
+			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
+			move->y_min_move = 1;
+			break;
+		}
+		case SDLK_KP_ENTER: {
+			//engine->context.demo = !engine->context.demo;
+			EntityIterator itlock;
+			search_entity_3(engine, COMP_ACTIVATABLE, COMP_ART, COMP_ACTIVATION, &itlock);
+			int next = 1;
+			if (next_entity(&itlock)) {
+				EntityId lockje = itlock.entity_id;
+				assert(lockje != NO_ENTITY);
+				ActivationComponent* aanmaken = get_component(engine, lockje, COMP_ACTIVATION);
+				next = 0;
+			}
+			if (next) {
+				ItemActionComponent* action = create_component(engine, player_entity_id, COMP_ITEMACTION);
+			}
+		}
+		case SDLK_RETURN: {
+			//engine->context.demo = !engine->context.demo;
+			EntityIterator itlock;
+			search_entity_3(engine, COMP_ACTIVATABLE, COMP_ART, COMP_ACTIVATION, &itlock);
+			int next = 1;
+			if (next_entity(&itlock)) {
+				EntityId lockje = itlock.entity_id;
+				assert(lockje != NO_ENTITY);
+				ActivationComponent* aanmaken = get_component(engine, lockje, COMP_ACTIVATION);
+				next = 0;
+			}
+			if (next) {
+				ItemActionComponent* action = create_component(engine, player_entity_id, COMP_ITEMACTION);
+			}
+			break;
+		}
+		default:
+			break;
+		}
+	}
+
+
+
 }
 
 void system_input_update(InputSystem* system, Engine* engine) {
-    EntityId input_recv_entity_id = search_first_entity_1(engine, COMP_INPUTRECEIVER);
+	EntityId input_recv_entity_id = search_first_entity_1(engine, COMP_INPUTRECEIVER);
+	CameraLookFromComponent* cameraLookFrom = search_first_component(engine, COMP_CAMERA_LOOK_FROM);
 
-    SDL_Event event;
-    memset(&event, 0, sizeof(SDL_Event));
-    /* Grab all the events off the queue. */
-    while( SDL_PollEvent( &event ) ) {
-        switch( event.type ) {
-            case SDL_KEYDOWN:
-				printf("up");
-                handleKeyDown(system, engine, &event.key.keysym, input_recv_entity_id);
-                break;
-            case SDL_KEYUP:
-                /* Handle key release. */
-				printf("tis ne graptje ");
-                handleKeyUp(system, engine, &event.key.keysym, input_recv_entity_id);
-                break;
-            case SDL_QUIT:
-                /* Handle quit requests (like Ctrl-c). */
-                engine->context.is_exit_game = 1;
-                break;
-            case SDL_MOUSEMOTION: {
-                SDL_MouseMotionEvent *mouseMotionEvent = (SDL_MouseMotionEvent *) &event;
-                //There are 2 mouse events we want to ignore:
-                // - initially, an event is generated that moves the mouse from 0,0 to the current position
-                // - then, each time SDL_WarpMouse is called, an event is generated for that call
-                if ((mouseMotionEvent->x != mouseMotionEvent->xrel || mouseMotionEvent->y != mouseMotionEvent->yrel)
-//                    &&
-//                    (mouseMotionEvent->x != system->screenMidX || mouseMotionEvent->y != system->screenMidY)
-                        ) {
-//                    SDL_WarpMouse(screenMidX, screenMidY);
+	SDL_Event event;
+	memset(&event, 0, sizeof(SDL_Event));
+	/* Grab all the events off the queue. */
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_KEYDOWN:
+			handleKeyDown(system, engine, &event.key.keysym, input_recv_entity_id);
+			break;
+		case SDL_KEYUP:
+			/* Handle key release. */
+			handleKeyUp(system, engine, &event.key.keysym, input_recv_entity_id);
+			break;
+		case SDL_QUIT:
+			/* Handle quit requests (like Ctrl-c). */
+			engine->context.is_exit_game = 1;
+			break;
+		case SDL_MOUSEMOTION: {
+			SDL_MouseMotionEvent *mouseMotionEvent = (SDL_MouseMotionEvent *) &event;
+			//There are 2 mouse events we want to ignore:
+			// - initially, an event is generated that moves the mouse from 0,0 to the current position
+			// - then, each time SDL_WarpMouse is called, an event is generated for that call
+			if ((mouseMotionEvent->x != mouseMotionEvent->xrel || mouseMotionEvent->y != mouseMotionEvent->yrel)
+				//                    &&
+				//                    (mouseMotionEvent->x != system->screenMidX || mouseMotionEvent->y != system->screenMidY)
+				) {
+				//                    SDL_WarpMouse(screenMidX, screenMidY);
 
-                    int buttonDown = mouseMotionEvent->state & SDL_BUTTON_LEFT;
-                    
-                    if (buttonDown) {
-						float x_move = mouseMotionEvent->xrel * 1.0f;
-						float y_move = mouseMotionEvent->yrel * 1.0f;
-                        printf("Mouse dragged %f %f\n", x_move, y_move);
-						CameraLookFromComponent* cameraLookFrom = search_first_component(engine, COMP_CAMERA_LOOK_FROM);
-						cameraLookFrom->XYdegees -= x_move;
-						cameraLookFrom->Zdegrees -= y_move;
-                    } else {
-                        //printf("Mouse moved %f %f\n", mouseMotionEvent->xrel * 1.0f, mouseMotionEvent->yrel * 1.0f);
-                    }
-                }
-                break;
-            }
-            case SDL_MOUSEBUTTONUP: {
-                //SDL 1.2 handles the wheel in a silly way
-                switch (event.button.button) {
-                    case SDL_BUTTON_WHEELUP: {
-                        printf("Wheel up\n");
-                        break;
-                    }
-                    case SDL_BUTTON_WHEELDOWN:{
-                        printf("Wheel down\n");
-                        break;
-                    }
-                }
-                break;
-            }
-        }
+				int buttonDown = mouseMotionEvent->state & SDL_BUTTON_LEFT;
 
-
-
-        //demo only, no use in real game
-        EntityIterator it;
-        search_entity_1(engine, COMP_ACTIVATABLE, &it);
-        while(next_entity(&it)) {
-            EntityId drawable_entity_id = it.entity_id;
-            assert(drawable_entity_id != NO_ENTITY);
-            ActivatableComponent* activatable = get_component(engine, drawable_entity_id, COMP_ACTIVATABLE);
-            activatable->active = engine->context.demo;
-        }
-    }
+				if (buttonDown) {
+					float x_move = mouseMotionEvent->xrel * 1.0f;
+					float y_move = mouseMotionEvent->yrel * 1.0f;
+					cameraLookFrom->XYdegees -= x_move;
+					cameraLookFrom->Zdegrees -= y_move;
+				}
+				else {
+					//printf("Mouse moved %f %f\n", mouseMotionEvent->xrel * 1.0f, mouseMotionEvent->yrel * 1.0f);
+				}
+			}
+			break;
+		}
+		case SDL_MOUSEBUTTONUP: {
+			//SDL 1.2 handles the wheel in a silly way
+			switch (event.button.button) {
+			case SDL_BUTTON_WHEELUP: {
+				if (cameraLookFrom->distance > 5) {
+					cameraLookFrom->distance -= 1;
+				}
+				break;
+			}
+			case SDL_BUTTON_WHEELDOWN: {
+				if (cameraLookFrom->distance < 25) {
+					cameraLookFrom->distance += 1;
+				}
+				break;
+			}
+			}
+			break;
+		}
+		}
+	}
 }
