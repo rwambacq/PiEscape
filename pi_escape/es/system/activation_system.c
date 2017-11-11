0600e75eb5a8f5aa070f47ead70dcebdf44fb6e8
thvkerck <Thomas.Vankerckhoven@UGent.be>
2017-11-11 18:17:04 +0100

    fix 1

--- pi_escape/es/system/activation_system.c
+++ pi_escape/es/system/activation_system.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdio.h>
-#include <sys\timeb.h> 
 
 ActivationSystem* system_activation_alloc() {
     ActivationSystem* res = calloc(1, sizeof(ActivationSystem));
