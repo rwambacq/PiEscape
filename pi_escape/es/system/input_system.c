4aa08f51f3b95b92d817477e1438ffa678f1c41c
thvkerck <Thomas.Vankerckhoven@UGent.be>
2017-11-11 23:28:20 +0100

    rev

--- pi_escape/es/system/input_system.c
+++ pi_escape/es/system/input_system.c
@@ -30,10 +30,10 @@ static void handleKeyDown(InputSystem* system, Engine* engine, SDL_keysym *keysy
         case SDLK_ESCAPE:
             //ignore untile key released
             break;
-		case SDLK_KP_ENTER: //fall-through
+        case SDLK_KP_ENTER: //fall-through
         case SDLK_RETURN:   //fall-through
         case SDLK_SPACE: {
-            //engine->context.demo = !engine->context.demo;
+            engine->context.demo = !engine->context.demo;
             break;
         }
         default:
@@ -47,99 +47,36 @@ static void handleKeyDown(InputSystem* system, Engine* engine, SDL_keysym *keysy
 
 static void handleKeyUp(InputSystem* system, Engine* engine, SDL_keysym *keysym, EntityId inputReceiverEntity)
 {
-	EntityIterator player_it;
-	EntityIterator itdoor;
-	search_entity_2(engine, COMP_GRIDLOCATION, COMP_INPUTRECEIVER, &player_it);
-	next_entity(&player_it);
-	EntityId player_entity_id = player_it.entity_id;
-	assert(player_entity_id != NO_ENTITY);
-	if (!has_component(engine, player_entity_id, COMP_BLOCKING)) {
-		switch (keysym->sym) {
-		case SDLK_o:
-			//key die deuren opent of sluit, handig voor debug
-			search_entity_3(engine, COMP_ART, COMP_ISDOOR, COMP_DIRECTION, &itdoor);
-			while (next_entity(&itdoor)) {
-				EntityId door = itdoor.entity_id;
-				assert(door != NO_ENTITY);
-				ActivatableComponent* xx = get_component(engine, door, COMP_ACTIVATABLE);
-				xx->active = !xx->active;
-			}
-			break;
-		case SDLK_ESCAPE:
-			engine->context.is_exit_game = 1;
-			break;
-		case SDLK_UP: {
-			//engine->context.demo = !engine->context.demo;
-			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
-			move->x_min_move = 1;
-			break;
-		}
-		case SDLK_DOWN: {
-			//engine->context.demo = !engine->context.demo;
-			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
-			move->x_plus_move = 1;
-			break;
-		}
-		case SDLK_LEFT: {
-			//engine->context.demo = !engine->context.demo;
-			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
-			move->y_plus_move = 1;
-			break;
-		}
-		case SDLK_RIGHT: {
-			//engine->context.demo = !engine->context.demo;
-			MoveActionComponent* move = create_component(engine, player_entity_id, COMP_MOVE_ACTION);
-			move->y_min_move = 1;
-			break;
-		}
-		case SDLK_KP_ENTER: {
-			//engine->context.demo = !engine->context.demo;
-			EntityIterator itlock;
-			search_entity_3(engine, COMP_ACTIVATABLE, COMP_ART, COMP_ACTIVATION, &itlock);
-			int next = 1;
-			if (next_entity(&itlock)) {
-				EntityId lockje = itlock.entity_id;
-				assert(lockje != NO_ENTITY);
-				ActivationComponent* aanmaken = get_component(engine, lockje, COMP_ACTIVATION);
-				if (aanmaken->currenttime == 50 || aanmaken->currenttime == 25) {
-					next = 0;
-				}
-			}
-			if (next) {
-				ItemActionComponent* action = create_component(engine, player_entity_id, COMP_ITEMACTION);
-			}
-		}
-		case SDLK_RETURN: {
-			//engine->context.demo = !engine->context.demo;
-			EntityIterator itlock;
-			search_entity_3(engine, COMP_ACTIVATABLE, COMP_ART, COMP_ACTIVATION, &itlock);
-			int next = 1;
-			if (next_entity(&itlock)) {
-				EntityId lockje = itlock.entity_id;
-				assert(lockje != NO_ENTITY);
-				ActivationComponent* aanmaken = get_component(engine, lockje, COMP_ACTIVATION);
-				if (aanmaken->currenttime == 25 || aanmaken->currenttime == 10) {
-					next = 0;
-				}
-			}
-			if (next) {
-				ItemActionComponent* action = create_component(engine, player_entity_id, COMP_ITEMACTION);
-			}
-			break;
-		}
-		default:
-			break;
-		}
-	}
-
+    switch( keysym->sym ) {
+        case SDLK_ESCAPE:
+            engine->context.is_exit_game = 1;
+            break;
+        case SDLK_UP:{
+            engine->context.demo = !engine->context.demo;
+            break;
+        }
+        case SDLK_DOWN:{
+            engine->context.demo = !engine->context.demo;
+            break;
+        }
+        case SDLK_LEFT:{
+            engine->context.demo = !engine->context.demo;
+            break;
+        }
+        case SDLK_RIGHT:{
+            engine->context.demo = !engine->context.demo;
+            break;
+        }
+        default:
+            break;
+    }
     
     
 }
 
 void system_input_update(InputSystem* system, Engine* engine) {
     EntityId input_recv_entity_id = search_first_entity_1(engine, COMP_INPUTRECEIVER);
-	CameraLookFromComponent* cameraLookFrom = search_first_component(engine, COMP_CAMERA_LOOK_FROM);
-	
+
     SDL_Event event;
     memset(&event, 0, sizeof(SDL_Event));
     /* Grab all the events off the queue. */
@@ -174,6 +111,8 @@ void system_input_update(InputSystem* system, Engine* engine) {
                     if (buttonDown) {
 						float x_move = mouseMotionEvent->xrel * 1.0f;
 						float y_move = mouseMotionEvent->yrel * 1.0f;
+                        printf("Mouse dragged %f %f\n", x_move, y_move);
+						CameraLookFromComponent* cameraLookFrom = search_first_component(engine, COMP_CAMERA_LOOK_FROM);
 						cameraLookFrom->XYdegees -= x_move;
 						cameraLookFrom->Zdegrees -= y_move;
                     } else {
@@ -186,20 +125,28 @@ void system_input_update(InputSystem* system, Engine* engine) {
                 //SDL 1.2 handles the wheel in a silly way
                 switch (event.button.button) {
                     case SDL_BUTTON_WHEELUP: {
-						if (cameraLookFrom->distance > 5) {
-							cameraLookFrom->distance -= 1;
-						}
+                        printf("Wheel up\n");
                         break;
                     }
                     case SDL_BUTTON_WHEELDOWN:{
-						if (cameraLookFrom->distance < 25) {
-							cameraLookFrom->distance += 1;
-						}
+                        printf("Wheel down\n");
                         break;
                     }
                 }
                 break;
             }
+        }
+
+
+
+        //demo only, no use in real game
+        EntityIterator it;
+        search_entity_1(engine, COMP_ACTIVATABLE, &it);
+        while(next_entity(&it)) {
+            EntityId drawable_entity_id = it.entity_id;
+            assert(drawable_entity_id != NO_ENTITY);
+            ActivatableComponent* activatable = get_component(engine, drawable_entity_id, COMP_ACTIVATABLE);
+            activatable->active = engine->context.demo;
         }
     }
 }
