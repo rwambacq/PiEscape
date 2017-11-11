28c18f0630fb3e0e72aecd36cf07f11e177a91d5
thvkerck <Thomas.Vankerckhoven@UGent.be>
2017-11-11 18:18:07 +0100

    fix 2

--- pi_escape/es/system/action_system.c
+++ pi_escape/es/system/action_system.c
@@ -4,21 +4,6 @@
 #include "../../../util/sleep.h"
 #include <stdlib.h>
 #include<stdio.h>
-#include <windows.h>
-#include <sys\timeb.h> 
-
-void usleep(__int64 usec)
-{
-	HANDLE timer;
-	LARGE_INTEGER ft;
-
-	ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative value indicates relative time
-
-	timer = CreateWaitableTimer(NULL, TRUE, NULL);
-	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
-	WaitForSingleObject(timer, INFINITE);
-	CloseHandle(timer);
-}
 
 
 
