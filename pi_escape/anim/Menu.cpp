2d9a5f0012a51c8080501437d191a88ea92ec4bc
rwambacq <ruben.wambacq@ugent.be>
2017-12-09 18:10:13 +0100

    model kijk regelaar

--- pi_escape/anim/Menu.cpp
+++ pi_escape/anim/Menu.cpp
@@ -6,7 +6,7 @@ void MenuDefinition::addMenuItem(MenuItem* item) {
 	this->menuItems.push_back(*item);
 }
 
-MenuItem::MenuItem(std::vector<GlyphDrawCommand> tekst, std::string action, std::vector<Animation> animations) {
+MenuItem::MenuItem(std::vector<GlyphDrawCommand> tekst, std::string action) {
 	this->tekst = tekst;
 	this->action = action;
 	this->selectedAnimations = animations;
