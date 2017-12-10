5af3723fc1b2d393383964febb999a525016622a
Jules <Jules.devos@ugent.be>
2017-12-10 11:50:47 +0100

    pi

--- pi_escape/imu/RTIMU_c_wrapper.h
+++ pi_escape/imu/RTIMU_c_wrapper.h
@@ -1,6 +1,12 @@
 #ifndef PIESCAPE2_RTIMU_C_WRAPPER_H
 #define PIESCAPE2_RTIMU_C_WRAPPER_H
 
-//TODO
+#include "RTIMULib.h"
+
+void sensor_imu_init();
+void check_new_imudata();
+float get_imu_x();
+float get_imu_y();
+float get_imu_z();
 
 #endif //PIESCAPE2_RTIMU_C_WRAPPER_H
