4b75fbde0398c36d831c4c6dd9dfe16dd14049e1
Jules <Jules.devos@ugent.be>
2017-12-10 18:33:50 +0100

    camera nu zou het moeten werken

--- pi_escape/es/system/orientation_system.c
+++ pi_escape/es/system/orientation_system.c
@@ -26,8 +26,14 @@ void system_orientation_update(OrientationSystem* system, Engine* engine) {
 	float value_z = get_imu_z();
 	if (system->sensor_x && system->sensor_z) {
 		cameraLookFrom->XYdegees = fmod((cameraLookFrom->XYdegees + (value_z - system->sensor_z) * 180 / M_PI), 360);
-		if (cameraLookFrom->Zdegrees >= (0.0f - (value_x - system->sensor_x) * 180 / M_PI) && cameraLookFrom->Zdegrees <= (90.0f - (value_x - system->sensor_x) * 180 / M_PI)) {
+		if (cameraLookFrom->Zdegrees >= 0.0f  && cameraLookFrom->Zdegrees <= 90.0f ) {
 			cameraLookFrom->Zdegrees -= (value_x - system->sensor_x) * 180 / M_PI;
+			if (cameraLookFrom->Zdegrees < 0.0f) {
+				cameraLookFrom->Zdegrees = 0.0f;
+			}
+			if (cameraLookFrom->Zdegrees > 90.0f) {
+				cameraLookFrom->Zdegrees = 90.0f;
+			}
 		}
 	}
 	system->sensor_x = value_x;
